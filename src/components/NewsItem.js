5840081be064491390efc3fd4930dd22a64590e2
Zabana <karim.cheurfi@gmail.com>
2019-09-25 16:33:36 +0200

    fetch and display stories based on the route

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import '../styles/newsitem.sass'
 
-const NewsItem = () => {
+const NewsItem = ({story}) => {
     return (
         <article className="newsItem">
             <header className="newsItem__header">
-                <h2><a href="/helo">Facebook to acquire startup for controlling computers with the mind.</a></h2>
+                <h2><a href={story.url}>{story.title}</a></h2>
             </header>
             <ul className="newsItem__info">
                 <li>
                     <i className="em em---1"></i> 
-                    <span>32</span>
+                    <span>{story.score}</span>
                 </li>
                 <li>
                     <i className="em em-adult"></i>
-                    <span>Bostonian</span> 
+                    <span>{story.by}</span> 
                 </li>
                 <li>
-                    <i class="em em-clock1"></i> 
-                    <span>4 hours ago</span>
+                    <i className="em em-clock1"></i> 
+                    <span>{story.time}</span>
                 </li>
                 <li>
-                    <i class="em em-left_speech_bubble"></i>
-                    <span>122</span>
+                    <i className="em em-left_speech_bubble"></i>
+                    <span>{story.descendants}</span>
                 </li>
             </ul>
         </article>
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,50 +1,86 @@
 import React from 'react'
 import NewsItem from './NewsItem'
 import '../styles/newslist.sass'
-import { Consumer } from '../context.js'
 import axios from 'axios';
-
-const arr = Array(30).fill(" ")
 
 class NewsList extends React.Component {
 
-    // https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
-    // https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty
-    // https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty
-    // https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty
-    // https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty
+    API_URL = 'https://hacker-news.firebaseio.com/v0/'
 
-    // show the first 30 then add pagination
+    state = {
+        stories: null
+    }
     
-    componentWillMount() {
-        // make the api call here
-        // use async await
-        // first log 
-        // then promise.all to get all the info about said stories
+    componentDidMount() {
+        this.fetchStoriesFromHNAPI()
+    }
+    
+    getStoryEndpoint() {
+        let endpoint
+        switch (this.props.location.pathname) {
+            case '/':
+                endpoint = 'topstories'
+                break;
+
+            case '/new':
+                endpoint = 'newstories'
+                break
+
+            case '/jobs':
+                endpoint = 'jobstories'
+                break
+
+            case '/ask':
+                endpoint = 'askstories'
+                break
+
+            case '/show':
+                endpoint = 'showstories'
+                break
+
+            default:
+                endpoint = 'topstories'
+        }
+        return endpoint
+    }
+
+    async fetchStoriesFromHNAPI() {
+        const endpoint = this.getStoryEndpoint()
+        const topstories =  await axios.get(`${this.API_URL}${endpoint}.json`)
+        const storyIds = topstories.data.slice(0, 30);
+        const stories = await Promise.all(storyIds.map( storyId => this.fetchStoryData(storyId)))
+        this.setState({stories: stories})
+    }
+
+    async fetchStoryData(storyId) {
+        const story = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+        return story.data
+    }
+
+    renderNewsList() {
+        return (
+            <main className="newsList">
+                {this.state.stories.map( (story, idx) => (
+                    <NewsItem key={idx} story={story} />
+                ))}
+            </main>
+        )
+    }
+
+    renderLoadingScreen() {
+        return (
+            <span>Loading ...</span>
+        )
     }
 
     render() {
-        return (
-            <Consumer>
-                { value => {
-                    return (
-                        <main className="newsList">
-                            hello
-                        </main>
-                    )
-                }}
-            </Consumer>
-        )
+        if (this.state.stories != null) {
+            return this.renderNewsList()
+        }
+        else {
+            return this.renderLoadingScreen()
+        }
     }
 }
-// const NewsList = () => (
-//     <main className="newsList">
-//         { arr.map( () => {
-//             return (
-//                 <NewsItem />
-//             )
-//         })}
-//     </main>
-// )
 
 export default NewsList
