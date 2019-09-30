21b3759a3ee476d52c0304d0e7ea9986974531b8
Zabana <karim.cheurfi@gmail.com>
2019-09-30 17:47:38 +0200

    remove console.log

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -101,7 +101,6 @@ class NewsList extends React.Component {
   renderNewsList() {
     return (
       <React.Fragment>
-        {console.log(this.state.stories)}
         <main className="newsList">
           {this.state.stories.map((story, idx) => (
             <NewsItem key={idx} id={this.computeStoryId(idx)} story={story} />
